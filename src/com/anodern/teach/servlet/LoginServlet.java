002b80d6f8c5f5c8dcde7fb4499ffd6626660678
anodern <jaznyang@hotmail.com>
2020-09-08 15:39:47 +0800

    test

--- src/com/anodern/teach/servlet/LoginServlet.java
+++ src/com/anodern/teach/servlet/LoginServlet.java
@@ -20,6 +20,7 @@ public class LoginServlet extends HttpServlet {
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out=response.getWriter();
         HttpSession session=request.getSession();
+        //git TEST
     
         String id = request.getParameter("id");
         if(id.equals("0")) {
